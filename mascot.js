e2f0e6f40f06b840260e71184df4d96ff4e8ade3
Shaw Tan <shawtan@gmail.com>
2015-08-06 13:04:53 -0400

    made seperate js file

---
+++ mascot.js
@@ -0,0 +1,34 @@
+var nameArray = [];
+var request = new XMLHttpRequest();
+request.onload = function (B) { 
+	return function () {
+
+		var fileLines = this.responseText.split('\n');
+		console.log(fileLines);
+		for (var i = fileLines.length - 1; i >= 0; i--) {
+			fileLines[i] = fileLines[i].split(',');
+		};
+		nameArray = fileLines;
+	}
+}(this);
+
+request.open('GET', 'names.txt');
+request.send();
+
+
+function mascotMaker(length) {
+	var rand1 = Math.floor((Math.random() * length));
+
+	while (true) {
+		var rand2 = Math.floor((Math.random() * length));
+
+		if (rand1 != rand2) {
+			break;
+		}
+	}
+	return nameArray[rand1][0] + nameArray[rand2][1] + " ("+rand1+","+rand2+")";
+}
+
+function newMascot() {
+	return document.getElementById('mascot').innerHTML=mascotMaker(nameArray.length-1);
+}
